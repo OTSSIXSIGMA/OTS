fce72cdbdc63d5ff61c56f0c4f0e538adffd1aa2
madhurikrishna <madhuriist@gmail.com>
2014-06-02 01:33:02 -0500

    Update StudentMenu.aspx.cs

--- StudentMenu.aspx.cs
+++ StudentMenu.aspx.cs
@@ -47,10 +47,7 @@ public partial class Menu : System.Web.UI.Page
         Question tmpQuestion;
         #endregion
 
-        connectionstring = @"Data Source=(LocalDB)\v11.0;" +
-          @"AttachDbFilename=|DataDirectory|\OnlineTraining.mdf;
-                Integrated Security=True;
-                Connect Timeout=30;";
+        connectionstring = "Server=55eb3ba5-c93f-4d5d-a746-a33d0187f51c.sqlserver.sequelizer.com;Database=db55eb3ba5c93f4d5da746a33d0187f51c;User ID=decjkfdwyfdldmsg;Password=joja5KVaS7pvgVztqJtWcVkv2Y2YyYpuUbbExi4FxeLA6UVjVXkFi5mvdVgfR5H2;";
         connection = new SqlConnection(connectionstring);
         cmd = new SqlCommand("que_GetQuestions", connection);
         cmd.CommandType = CommandType.StoredProcedure;
@@ -78,10 +75,7 @@ public partial class Menu : System.Web.UI.Page
         Option tmpOption;
         #endregion
 
-        connectionstring = @"Data Source=(LocalDB)\v11.0;" +
-          @"AttachDbFilename=|DataDirectory|\OnlineTraining.mdf;
-                Integrated Security=True;
-                Connect Timeout=30;";
+        connectionstring = "Server=55eb3ba5-c93f-4d5d-a746-a33d0187f51c.sqlserver.sequelizer.com;Database=db55eb3ba5c93f4d5da746a33d0187f51c;User ID=decjkfdwyfdldmsg;Password=joja5KVaS7pvgVztqJtWcVkv2Y2YyYpuUbbExi4FxeLA6UVjVXkFi5mvdVgfR5H2;";
         connection = new SqlConnection(connectionstring);
         cmd = new SqlCommand("opt_GetOptions", connection);
         cmd.CommandType = CommandType.StoredProcedure;
