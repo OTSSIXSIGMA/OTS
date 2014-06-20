d2e5d64d43eb49f84415f80f5d0b8db9ed6b6a01
madhurikrishna <madhuriist@gmail.com>
2014-06-20 10:15:44 -0500

    v3
    v3

--- Reporting.aspx.cs
+++ Reporting.aspx.cs
@@ -22,16 +22,30 @@ public partial class Reporting : System.Web.UI.Page
         List<OnlineTrainingBL.Report> report = new List<OnlineTrainingBL.Report>();
         foreach (Question question in ((List<Question>)Session["Questions"]))
         {
-            if(!string.IsNullOrEmpty( question.SelectedList))
+            if (!string.IsNullOrEmpty(question.SelectedList))
             {
-               List<String> OptionsOrder = new List<String>(question.SelectedList.Split(','));
-               string OptionChosen = question.OptionList.Find(q => q.ID == Convert.ToInt16(OptionsOrder.ElementAt(OptionsOrder.Count - 1))).DisplayID;
-               string OptionCorrect = question.OptionList.Find(q => q.isAnswer == true).DisplayID;
-                Report tmpReport = new Report(question.DisplayID.ToString(), OptionChosen, OptionCorrect, (OptionChosen==OptionCorrect)?"Correct":"Wrong");
-               report.Add(tmpReport);
+                List<String> OptionsOrder = new List<String>(question.SelectedList.Split(','));
+                string OptionChosen = question.OptionList.Find(q => q.ID == Convert.ToInt16(OptionsOrder.ElementAt(OptionsOrder.Count - 1))).DisplayID;
+                string OptionCorrect = question.OptionList.Find(q => q.isAnswer == true).DisplayID;
+                Report tmpReport = new Report(question.DisplayID.ToString(), OptionChosen, OptionCorrect, (OptionChosen == OptionCorrect) ? "Correct" : "Wrong");
+                report.Add(tmpReport);
             }
         }
 
+        int numberCorrect = 0;
+        foreach (Question question in ((List<Question>)Session["Questions"]).FindAll(x => x.SelectedList != null))
+        {
+            List<string> OptionsOrder = new List<string>(question.SelectedList.Split(','));
+            string Chosen = question.OptionList.Find(q => q.ID == Convert.ToInt16(OptionsOrder.ElementAt(OptionsOrder.Count - 1))).Value.Replace("[", "").Replace("]", "");
+            string Correct = question.OptionList.Find(q => q.isAnswer == true).Value.Replace("[", "").Replace("]", "");
+            if (Chosen == Correct)
+            {
+                numberCorrect++;
+            }
+
+        }
+        double Accuracy = ((double)numberCorrect / (((List<Question>)Session["Questions"]).FindAll(x => x.SelectedList != null).Count)) * 100;
+        lblReport.Text = "<br/>Accuracy =" + Accuracy + "%";
         if (report.Count != 0)
         {
             gdvReport.DataSource = report;
