0b39c6e5b6702fbb3f2575d2f670a4eb85f2ae66
madhurikrishna <madhuriist@gmail.com>
2014-07-22 18:30:19 -0500

    Update TestQuestion.aspx.cs

--- TestQuestion.aspx.cs
+++ TestQuestion.aspx.cs
@@ -79,7 +79,7 @@ public partial class TestQuestion : System.Web.UI.Page
                 foreach (string match in matches)
                 {
                     string wildcard = "[" + match + "]";
-                    lblQuestion.Text = lblQuestion.Text.Replace(wildcard, "<a href='Description.aspx?name=" + match + "' rel=shadowbox;height=700;width=700>" + match + "</a>");
+                    lblQuestion.Text = lblQuestion.Text.Replace(wildcard, "<a href='Description.aspx?name=" + match + "' rel=shadowbox;height=1000;width=1000>" + match + "</a>");
                 }
 
                 foreach (Option option in question.OptionList)
