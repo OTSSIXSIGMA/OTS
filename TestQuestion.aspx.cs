f614f77cb86869978c72befb530d5ae0dd46b4be
madhurikrishna <madhuriist@gmail.com>
2014-07-25 03:29:54 -0500

    Update TestQuestion.aspx.cs

--- TestQuestion.aspx.cs
+++ TestQuestion.aspx.cs
@@ -53,9 +53,11 @@ public partial class TestQuestion : System.Web.UI.Page
             }
 
                 question = ((List<Question>)Session["Questions"]).Find(q => q.ID == QuestionID);
+                question.Value= "<b>"+question.Value+"</b>";
 
             if (question.ID < 0)
             {
+                
                 lblQuestion.Text = question.Value;
                 ICollection<string> matches =
                 Regex.Matches(question.Value.Replace(Environment.NewLine, ""), @"\[([^]]*)\]")
